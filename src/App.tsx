cb71730d91b97a004be9aaaf2d0923c7dedb6f61
hao <klaushaozhang@gmail.com>
2022-06-16 09:46:30 +1200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useLayoutEffect } from 'react'
+
+import { useQuery } from '@apollo/client'
+import styled from 'styled-components'
+import { gql } from '@apollo/client'
+
+export const avatarQuery = gql`
+  query MarketPlaceQuery {
+    marketplaceListings(first: 40) {
+      edges {
+        cursor
+        node {
+          logoUrl
+          name
+        }
+      }
+    }
+  }
+`
+
+
+const padding = 5
+
+const AppWrapper = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+const Row = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+})
+
+export const iconWidth = 120
+
+export const borderWidth = 1
+export const cardWidth = 120 + borderWidth * 2
+
+const Wrapper = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  border: `${borderWidth}px solid #ccc`,
+  width: iconWidth,
+})
+
+const ImageWrapper = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding,
+})
+
+const Image = styled.img({
+  width: iconWidth - padding * 2,
+  height: iconWidth - padding * 2,
+})
+
+const Title = styled.h5({
+  overflow: 'hidden',
+})
 
 function App() {
+  const { loading, error, data } = useQuery(avatarQuery)
+  const [[columCount, rowCount], setSize] = useState([0, 0])
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      if (loading) return
+      if (error) return
+      const columCount = Math.floor(window.innerWidth / cardWidth)
+      const rowCount = Math.ceil(
+        data.marketplaceListings.edges.length / columCount
+      )
+
+      setSize([columCount, rowCount])
+    }
+    window.addEventListener('resize', updateSize)
+    updateSize()
+    return () => window.removeEventListener('resize', updateSize)
+  }, [data, loading, error])
+
+  if (loading) return <div>loading...</div>
+  if (error) return <div>error</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppWrapper>
+      {Array.from({ length: rowCount }).map((_, rowIndex) => {
+        return (
+          <Row key={rowIndex}>
+            {data.marketplaceListings.edges
+              .slice(rowIndex * columCount, (rowIndex + 1) * columCount)
+              .map((d: any) => {
+                const { logoUrl, name } = d.node
+                return (
+                  <Wrapper key={d.cursor}>
+                    <Title>{name}</Title>
+                    <ImageWrapper>
+                      <Image src={logoUrl} alt="avatar" />
+                    </ImageWrapper>
+                  </Wrapper>
+                )
+              })}
+          </Row>
+        )
+      })}
+    </AppWrapper>
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+    },
+  }
+})
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
