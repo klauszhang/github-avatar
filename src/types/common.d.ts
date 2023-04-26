1447e01cfc1c52082f345e21cf9331dbe4097249
carl0sarb0leda <carlosaepn@gmail.com>
2023-04-26 19:37:41 +1200

    Add common types from github docs

---
+++ src/types/common.d.ts
@@ -0,0 +1,20 @@
+//Types from https://docs.github.com/en/graphql/reference/objects
+export type PageInfo = {
+  hasNextPage: boolean
+  endCursor: string
+}
+
+//It implements generic types to make it more flexible
+export type Edge<T> = {
+  cursor: string
+  node: T
+}
+
+//Marketplace listings data
+export type DataFields<T> = {
+  marketplaceListings: {
+    edges: Edge<T>[]
+    pageInfo: PageInfo
+    __typename: string
+  }
+}
